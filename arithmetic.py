ba3f7a7129a35bc2a08aedefc01f8d85767dd09c
Minnu John <minnu.john@snhu.edu>
2023-05-18 16:36:57 -0500

    fix error

--- arithmetic.py
+++ arithmetic.py
@@ -33,5 +33,5 @@ def power(num1, num2):
     return pow(num1,num2)
 
 def mod(num1, num2):
-    """Return the remainder of num1 / num2."""
+    """Return the remainder of num1 % num2."""
     return num1 % num2
